80550b33c29618f38759856fd45063451b8d2d13
seireitou <ausis@mail.com>
2021-03-01 16:30:36 -0600

    commit tree 51bbb3490fa35413cd3c445933691c61bf04b8ea

---
+++ app/controllers/application_controller copy.rb
@@ -0,0 +1,64 @@
+# class ApplicationController < Sinatra::Base
+#   configure do
+#     set :public_folder, 'public'
+#     set :views, 'app/views'
+#   end
+
+#   # code actions here!
+
+#   get '/' do
+#   end
+
+#   get '/recipes' do
+#     @recipes = Recipe.all
+
+#     erb :index
+#   end
+
+#   get '/recipes/new' do
+#     @recipe = Recipe.new
+
+#     erb :new
+#   end
+
+#   post '/recipes' do
+#     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+
+#     redirect "/recipes/#{@recipe.id}"
+#   end
+
+#   get '/recipes/:id' do
+#     @recipe = Recipe.find_by_id(params[:id])
+#     @recipe_name = @recipe.name
+#     @recipe_ingredients = @recipe.ingredients
+#     @recipe_cook_time = @recipe.cook_time
+#     @recipe_id = @recipe.id
+
+#     erb :show
+#   end
+
+#   get '/recipes/:id/edit' do
+#     @recipe = Recipe.find_by(id: params[:id])
+#     @recipe_name = @recipe.name
+#     @recipe_ingredients = @recipe.ingredients
+#     @recipe_cook_time = @recipe.cook_time
+#     @recipe_id = @recipe.id
+
+#     erb :edit
+#   end
+
+#   patch '/recipes/:id' do
+#     @recipe = Recipe.find_by(id: params[:id])
+#     @recipe.update(params)
+#     binding.pry
+#     erb :show
+#   end
+
+#   delete '/recipes/:id' do
+#     @recipe = Recipe.find_by(id: params[:id])
+#     @recipe.delete
+
+#     redirect '/recipes'
+#   end
+
+# end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,58 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do
+    redirect to '/recipes'
+  end
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    puts params
+    @recipe = Recipe.create(params)
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+  
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe_name = @recipe.name
+    @recipe_ingredients = @recipe.ingredients
+    @recipe_cook_time = @recipe.cook_time
+    @recipe_id = @recipe.id
+
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe_name = @recipe.name
+    @recipe_ingredients = @recipe.ingredients
+    @recipe_cook_time = @recipe.cook_time
+    @recipe_id = @recipe.id
+
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.update(params)
+
+    erb :show
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.delete
+
+    redirect to '/recipes'
+  end
 end
